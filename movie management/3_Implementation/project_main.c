70cfa240421005e2ecc47888f0d37c60ec868f81
99002531 <71481297+99002531@users.noreply.github.com>
2020-09-19 07:46:58 +0530

    Update project_main.c
    main c

--- movie management/3_Implementation/project_main.c
+++ movie management/3_Implementation/project_main.c
@@ -1,112 +1,60 @@
-#include <calculator_operations.h>
 
-/* Status of the operation requested */
-#define VALID   (1)
-#define INVALID (0)
-
-/* Calculator operation requested by user*/
-unsigned int calculator_operation = 0;
-
-/* Operands on which calculation is performed */
-int calculator_operand1 = 0;
-int calculator_operand2 = 0;
-
-/* Valid operations */
-enum operations{ ADD=1, SUBTRACT, MULTIPLY, DIVIDE, EXIT };
-
-/* Display the menu of operations supported */
-void calculator_menu(void);
-/* Verifies the requested operations validity */
-int valid_operation(int operation);
-
-
-/* Start of the application */
-int main(int argc, char *argv[])
+#include "sdlc.h"
+int main()
 {
-    printf("\n****Calculator****\n");
-    while(1)
-    {
-        calculator_menu();
-    }
-}
+	int ch;
 
-void calculator_menu(void)
-{
-    printf("\nAvailable Operations\n");
-    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit");
-    printf("\n\tEnter your choice\n");
-   
-     __fpurge(stdin);
-    scanf("%d", &calculator_operation);
+ 	do{
+	printf("\n====================================================================");
+	printf("\n");
+	printf("\t Movie or series finding ");
+	printf("\n");
+	printf("\n====================================================================");
 
-    if(EXIT == calculator_operation)
-    {
-        printf("\nThank you. Exiting the Application\n");
-        exit(0);
-    }
+	printf("\nPress <1> Insert Movie or series");
+  printf("\nPress <2> View All Movie or series");
+	printf("\nPress <3> Find Movie ");
+	printf("\nPress <4> For doing subscriptions of Movie or series ");
+	printf("\nPress <5> For seeing old subscriptions of Movie or series ");
 
-    if(INVALID != valid_operation(calculator_operation))
-    {
-        printf("\n\tEnter your Numbers with space between them\n");
-        __fpurge(stdin);
-        scanf("%d %d", &calculator_operand1, &calculator_operand2);
-    }
-    else
-    {
-        printf("\n\t---Wrong choice---\nEnter to continue\n");
-        __fpurge(stdin);
-        getchar();
-        return;
-        
-    }
-    switch(calculator_operation)
-    {
-        case ADD:
-            printf("\n\t%d + %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            add(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
-            break;
-        case SUBTRACT:
-            printf("\n\t%d - %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            subtract(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
-            break;
-        case MULTIPLY:
-            printf("\n\t%d * %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            multiply(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
-            break;
-        case DIVIDE:
-            printf("\n\t%d / %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            divide(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
-            break;
-        case 5:
-            exit(0);
-            break;
-        default:
-            printf("\n\t---It should never come here---\n");
-    }
-}
+   	printf("\nPress <0> Exit ");
 
-int valid_operation(int operation)
-{
-    /* Check if the operation is a valid operation */
-    return ((ADD <= operation) && (EXIT >= operation)) ? VALID: INVALID;
+   	printf("\nEnter your Choice ::");
+
+		__fpurge(stdin);
+   	scanf("%d",&ch);
+
+   	switch (ch)
+   	{
+    		case 1 :
+    		insert_details();
+   	   	break;
+
+		    case 2:
+    		viewAll();
+   		  break;
+
+		    case 3:
+    		find();
+   		  break;
+
+			  case 4:
+				subscriptions();
+				break;
+
+				case 5:
+				old_record();
+				break;
+
+        case 0:
+    		exit(0);
+    		break;
+
+    		default:
+    		printf("Wrong choice !");
+    		break;
+   	}
+ }while(ch!=0);
+
+return 0;
 }
